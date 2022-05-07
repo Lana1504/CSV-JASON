22f39c11290352a59b27267c1731c40d0b548715
Lana <101338367+Lana1504@users.noreply.github.com>
2022-05-07 20:07:01 +0300

    JUNIT1

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,83 @@
+import org.junit.jupiter.api.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestTest extends Main {
+
+
+    @Test
+    @Order(1)
+    public void listToJson() {
+        String actual = listToJson(new ArrayList<> ());
+        assertInstanceOf(String.class, actual);
+        assertNotNull(actual);
+        System.out.println("Test # 1 listToJson() passed");
+    }
+
+    @Test
+    @Order(2)
+    @Timeout(value = 160, unit = TimeUnit.MILLISECONDS)
+    void writeString() throws ParserConfigurationException, IOException, SAXException {
+
+        String expected = listToJson(parseXML("data.xml"));
+        String actual = "[{\"id\":1,\"firstName\":\"John\",\"lastName\":\"Smith\",\"country\":\"USA\",\"age\":25}," +
+                "{\"id\":2,\"firstName\":\"Ivan\",\"lastName\":\"Petrov\",\"country\":\"RU\",\"age\":23}]";
+
+        assertTrue(expected.contains("[{"));
+        assertNotNull(expected);
+        assertNotNull(actual);
+        assertInstanceOf(String.class, expected);
+        assertInstanceOf(String.class, actual);
+        assertThrows(IOException.class, () -> {
+            throw new IOException();
+        });
+        System.out.println("Test # 2 writeString() passed");
+    }
+
+    @Test
+    @Order(3)
+    void parseXML() throws ParserConfigurationException, IOException, SAXException {
+
+        Employee emp1 = new Employee(1, "John", "Smith", "USA", 25);
+        Employee emp2 = new Employee(2, "Ivan", "Petrov", "RU", 23);
+        List<Employee> expected = new ArrayList<>();
+        expected.add(emp1);
+        expected.add(emp2);
+        List<Employee> actual = parseXML("data.xml");
+
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.get(i).id, actual.get(i).id);
+            assertEquals(expected.get(i).firstName, actual.get(i).firstName);
+            assertEquals(expected.get(i).lastName, actual.get(i).lastName);
+            assertEquals(expected.get(i).country, actual.get(i).country);
+            assertEquals(expected.get(i).age, actual.get(i).age);
+        }
+        assertInstanceOf(List.class, actual);
+        assertInstanceOf(List.class, expected);
+        assertEquals(expected.size(), 2);
+        assertNotSame(expected, actual);
+        assertNotEquals(expected, actual);
+        assertNotNull(actual);
+        assertNotNull(expected);
+
+        System.out.println("Expected " + expected);
+        System.out.println("Actual " + actual);
+        System.out.println("Test # 3 parseXML() passed");
+    }
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
